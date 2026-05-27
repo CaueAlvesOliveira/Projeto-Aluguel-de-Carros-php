f63705114b4185fb5325d0f5bdd45f55b67e4ef8
Brayanhnk <brayanhoinacki@gmail.com>
2026-05-27 20:35:37 -0300

    feat: Seguranca injecao XSS e validacao email

--- contato.php
+++ contato.php
@@ -12,6 +12,8 @@
         // Validação simples
         if (empty($nome) || empty($email) || empty($assunto) || empty($mensagemUsuario)) {
             $mensagem = "Por favor, preencha todos os campos.";
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $mensagem = "Por favor, insira um email válido.";
         } else {
             // Configurações do email
             $destinatario = "destinatarioemail@gmail.com";
@@ -59,22 +61,22 @@
         <h1>Contato</h1>
 
         <?php if (!empty($mensagem)) : ?>
-            <div class="alert alert-info"><?php echo $mensagem; ?></div>
+            <div class="alert alert-info"><?php echo htmlspecialchars($mensagem); ?></div>
         <?php endif; ?>
 
         <form method="POST">
 
         <label>Nome</label>
-        <input type="text" name="nome" required value="<?= $nome ?? '' ?>">
+        <input type="text" name="nome" required value="<?= htmlspecialchars($nome ?? '') ?>">
 
         <label>Email</label>
-        <input type="email" name="email" required value="<?= $email ?? '' ?>">
+        <input type="email" name="email" required value="<?= htmlspecialchars($email ?? '') ?>">
 
         <label>Assunto</label>
-        <input type="text" name="assunto" required value="<?= $assunto ?? '' ?>">
+        <input type="text" name="assunto" required value="<?= htmlspecialchars($assunto ?? '') ?>">
 
         <label>Mensagem</label>
-        <textarea name="mensagem" required><?= $mensagemUsuario ?? '' ?></textarea>
+        <textarea name="mensagem" required><?= htmlspecialchars($mensagemUsuario ?? '') ?></textarea>
 
         <button type="submit">Enviar</button>
 
